38ba1e697346cba63cbdde05e95309573546b4c9
oonpupu <joowon0603.seo@gmail.com>
2026-02-02 17:03:48 +0900

    error fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import { useState } from "react";
 export default function Home() {
   const [text, setText] = useState("");
 
-  function showInfo(item) {
+  function showInfo(item: string) {
+    {
     if (item === "plastic") setText("Rinse and recycle in plastic bin.");
     if (item === "paper") setText("Remove staples and recycle as paper.");
     if (item === "can") setText("Rinse and recycle with metal cans.");
